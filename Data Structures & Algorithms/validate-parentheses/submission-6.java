6a8cb7b0c805e7f549cad294f982872984891ad4
VMahamadhBasha <vmahamadhbasha@gmail.com>
2026-07-31 18:19:49 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        Set<Character> d=new HashSet<>();
+        
+        for(char i:s.toCharArray()){
+                if(i=='('||i=='['||i=='{'){
+                    st.add(i);
+                }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char c=st.pop();
+                if((i==')' && c!='(')||(i=='}' && c!='{')||(i==']' && c!='[')){
+                    return false;
+                }
+                }
+        }
+        return true;
+        
+    }
+}
